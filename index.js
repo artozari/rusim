e00e45316428f7ebf7080586a108a23c132f96e4
artozari <artozari@gmail.com>
2024-12-03 17:59:17 -0300

    envia datos  actualizados de status, noSmoking y demas

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ let cantMesas = 5; //# La cantidad de mesas a publicar
 let gameNumber = 1;
 let tableDataNew = [...jsonfile.tableData];
 let configDataNew = [...jsonfile.configData];
+let status = [];
+let casinoDataNew = [...jsonfile.casinoData];
 let idDataBase = 1;
 let maxHistory = 150;
 let x = 0;
@@ -59,6 +61,9 @@ setInterval(() => {
 function modJson(datajson, i) {
   let currently = Date.now();
 
+  casinoDataNew[14] = "cantPlanos";
+  casinoDataNew[15] = Math.floor(Math.random() * 3) + 1;
+
   tableDataNew[1] = i;
   tableDataNew[3] = `Table ${i.toString().padStart(2, 0)}`;
   tableDataNew[5] = `t${i.toString().padStart(2, "0")}`;
@@ -68,10 +73,21 @@ function modJson(datajson, i) {
   tableDataNew[9] = x;
   tableDataNew[10] = "positionY";
   tableDataNew[11] = y;
+  tableDataNew[12] = "layout";
+  tableDataNew[13] = 0;
+  tableDataNew[14] = "noSmoking";
+  tableDataNew[15] = true;
 
   configDataNew[3] = i;
-  configDataNew[32] = "status";
-  configDataNew[33] = seleccionarColorAleatorio();
+  configDataNew[32] = "Language";
+  configDataNew[33] = "es";
+  configDataNew[34] = "Language-2";
+  configDataNew[35] = "en";
+  configDataNew[36] = "Language-3";
+  configDataNew[37] = "off";
+
+  status[0] = "semaphore";
+  status[1] = seleccionarColorAleatorio();
 
   if (winningNumberArray[i - 1].length >= maxHistory) {
     winningNumberArray[i - 1].pop();
@@ -95,9 +111,11 @@ function modJson(datajson, i) {
     ...datajson,
     ts: currently,
     gameNumber: gameNumber,
+    casinoData: casinoDataNew,
     tableData: tableDataNew,
     configData: configDataNew,
     winningNumbersData: winningNumberArray[i - 1],
+    status: status,
   };
 
   return nuevoDataJason;
@@ -113,8 +131,6 @@ function seleccionarColorAleatorio() {
   const indiceAleatorio = Math.floor(Math.random() * valores.length);
   return valores[indiceAleatorio];
 }
-
-
 
 client.on("message", (topic, message) => {
   console.log(`Received message on topic ${topic}: ${message.toString()}`);
